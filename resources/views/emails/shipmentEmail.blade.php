00aa5bcf008b18025d422242af3a19c6f42ec273
godwininyene <godwinhigh2@gmail.com>
2024-05-28 03:09:56 +0100

    WIP: Change the email template.

--- resources/views/emails/shipmentEmail.blade.php
+++ resources/views/emails/shipmentEmail.blade.php
@@ -1,535 +1,64 @@
-<!DOCTYPE html
-    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
+<!DOCTYPE html>
+<html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="x-apple-disable-message-reformatting" />
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="color-scheme" content="light dark" />
-    <meta name="supported-color-schemes" content="light dark" />
-    <title></title>
-    <style type="text/css" rel="stylesheet" media="all">
-        /* Base ------------------------------ */
-
-        @import url("https://fonts.googleapis.com/css?family=Nunito+Sans:400,700&display=swap");
-
+    <title>Your Package from {{$sender}} Has Been Received</title>
+    <style>
         body {
-            width: 100% !important;
-            height: 100%;
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
             margin: 0;
-            -webkit-text-size-adjust: none;
+            padding: 0;
+            background-color: #f4f4f4;
         }
-
-        a {
-            color: #3869D4;
+        .container {
+            max-width: 600px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #ffffff;
+            border: 1px solid #dddddd;
         }
-
-        a img {
-            border: none;
-        }
-
-        td {
-            word-break: break-word;
-        }
-
-        .preheader {
-            display: none !important;
-            visibility: hidden;
-            mso-hide: all;
-            font-size: 1px;
-            line-height: 1px;
-            max-height: 0;
-            max-width: 0;
-            opacity: 0;
-            overflow: hidden;
-        }
-
-        /* Type ------------------------------ */
-
-        body,
-        td,
-        th {
-            font-family: "Nunito Sans", Helvetica, Arial, sans-serif;
-        }
-
-        h1 {
-            margin-top: 0;
-            color: #333333;
-            font-size: 22px;
-            font-weight: bold;
-            text-align: left;
-        }
-
-        h2 {
-            margin-top: 0;
-            color: #333333;
-            font-size: 16px;
-            font-weight: bold;
-            text-align: left;
-        }
-
-        h3 {
-            margin-top: 0;
-            color: #333333;
-            font-size: 14px;
-            font-weight: bold;
-            text-align: left;
-        }
-
-        td,
-        th {
-            font-size: 16px;
-        }
-
-        p,
-        ul,
-        ol,
-        blockquote {
-            margin: .4em 0 1.1875em;
-            font-size: 16px;
-            line-height: 1.625;
-        }
-
-        p.sub {
-            font-size: 13px;
-        }
-
-        /* Utilities ------------------------------ */
-
-        .align-right {
-            text-align: right;
-        }
-
-        .align-left {
-            text-align: left;
-        }
-
-        .align-center {
+        .header, .footer {
             text-align: center;
         }
-
-        /* Buttons ------------------------------ */
-
+        .content {
+            margin: 20px 0;
+        }
         .button {
-            background-color: #3869D4;
-            border-top: 10px solid #3869D4;
-            border-right: 18px solid #3869D4;
-            border-bottom: 10px solid #3869D4;
-            border-left: 18px solid #3869D4;
             display: inline-block;
-            color: #FFF;
-            text-decoration: none;
-            border-radius: 3px;
-            box-shadow: 0 2px 3px rgba(0, 0, 0, 0.16);
-            -webkit-text-size-adjust: none;
-            box-sizing: border-box;
-        }
-
-        .button--yellow {
-            background-color: #facc15;
-            border-top: 10px solid #facc15;
-            border-right: 18px solid #facc15;
-            border-bottom: 10px solid #facc15;
-            border-left: 18px solid #facc15;
-        }
-
-        .button--red {
-            background-color: #FF6136;
-            border-top: 10px solid #FF6136;
-            border-right: 18px solid #FF6136;
-            border-bottom: 10px solid #FF6136;
-            border-left: 18px solid #FF6136;
-        }
-
-        @media only screen and (max-width: 500px) {
-            .button {
-                width: 100% !important;
-                text-align: center !important;
-            }
-        }
-
-        /* Attribute list ------------------------------ */
-
-        .attributes {
-            margin: 0 0 21px;
-        }
-
-        .attributes_content {
-            background-color: #F4F4F7;
-            padding: 16px;
-        }
-
-        .attributes_item {
-            padding: 0;
-        }
-
-        /* Related Items ------------------------------ */
-
-        .related {
-            width: 100%;
-            margin: 0;
-            padding: 25px 0 0 0;
-            -premailer-width: 100%;
-            -premailer-cellpadding: 0;
-            -premailer-cellspacing: 0;
-        }
-
-        .related_item {
-            padding: 10px 0;
-            color: #CBCCCF;
-            font-size: 15px;
-            line-height: 18px;
-        }
-
-        .related_item-title {
-            display: block;
-            margin: .5em 0 0;
-        }
-
-        .related_item-thumb {
-            display: block;
-            padding-bottom: 10px;
-        }
-
-        .related_heading {
-            border-top: 1px solid #CBCCCF;
-            text-align: center;
-            padding: 25px 0 10px;
-        }
-
-        /* Discount Code ------------------------------ */
-
-        .discount {
-            width: 100%;
-            margin: 0;
-            padding: 24px;
-            -premailer-width: 100%;
-            -premailer-cellpadding: 0;
-            -premailer-cellspacing: 0;
-            background-color: #F4F4F7;
-            border: 2px dashed #CBCCCF;
-        }
-
-        .discount_heading {
-            text-align: center;
-        }
-
-        .discount_body {
-            text-align: center;
-            font-size: 15px;
-        }
-
-        /* Social Icons ------------------------------ */
-
-        .social {
-            width: auto;
-        }
-
-        .social td {
-            padding: 0;
-            width: auto;
-        }
-
-        .social_icon {
-            height: 20px;
-            margin: 0 8px 10px 8px;
-            padding: 0;
-        }
-
-        /* Data table ------------------------------ */
-
-        .purchase {
-            width: 100%;
-            margin: 0;
-            padding: 35px 0;
-            -premailer-width: 100%;
-            -premailer-cellpadding: 0;
-            -premailer-cellspacing: 0;
-        }
-
-        .purchase_content {
-            width: 100%;
-            margin: 0;
-            padding: 25px 0 0 0;
-            -premailer-width: 100%;
-            -premailer-cellpadding: 0;
-            -premailer-cellspacing: 0;
-        }
-
-        .purchase_item {
-            padding: 10px 0;
-            color: #51545E;
-            font-size: 15px;
-            line-height: 18px;
-        }
-
-        .purchase_heading {
-            padding-bottom: 8px;
-            border-bottom: 1px solid #EAEAEC;
-        }
-
-        .purchase_heading p {
-            margin: 0;
-            color: #85878E;
-            font-size: 12px;
-        }
-
-        .purchase_footer {
-            padding-top: 15px;
-            border-top: 1px solid #EAEAEC;
-        }
-
-        .purchase_total {
-            margin: 0;
-            text-align: right;
-            font-weight: bold;
-            color: #333333;
-        }
-
-        .purchase_total--label {
-            padding: 0 15px 0 0;
-        }
-
-        body {
-            background-color: #F4F4F7;
-            color: #51545E;
-        }
-
-        p {
-            color: #51545E;
-        }
-
-        p.sub {
-            color: #6B6E76;
-        }
-
-        .email-wrapper {
-            width: 100%;
-            margin: 0;
-            padding: 0;
-            -premailer-width: 100%;
-            -premailer-cellpadding: 0;
-            -premailer-cellspacing: 0;
-            background-color: #F4F4F7;
-        }
-
-        .email-content {
-            width: 100%;
-            margin: 0;
-            padding: 0;
-            -premailer-width: 100%;
-            -premailer-cellpadding: 0;
-            -premailer-cellspacing: 0;
-        }
-
-        /* Masthead ----------------------- */
-
-        .email-masthead {
-            padding: 25px 0;
-            text-align: center;
-        }
-
-        .email-masthead_logo {
-            width: 94px;
-        }
-
-        .email-masthead_name {
+            padding: 10px 20px;
             font-size: 16px;
-            font-weight: bold;
-            color: #A8AAAF;
+            color: #ffffff;
+            background-color: #007bff;
             text-decoration: none;
-            text-shadow: 0 1px 0 white;
+            border-radius: 5px;
         }
-
-        /* Body ------------------------------ */
-
-        .email-body {
-            width: 100%;
-            margin: 0;
-            padding: 0;
-            -premailer-width: 100%;
-            -premailer-cellpadding: 0;
-            -premailer-cellspacing: 0;
-            background-color: #FFFFFF;
+        .footer p {
+            font-size: 12px;
+            color: #666666;
         }
-
-        .email-body_inner {
-            width: 570px;
-            margin: 0 auto;
-            padding: 0;
-            -premailer-width: 570px;
-            -premailer-cellpadding: 0;
-            -premailer-cellspacing: 0;
-            background-color: #FFFFFF;
-        }
-
-        .email-footer {
-            width: 570px;
-            margin: 0 auto;
-            padding: 0;
-            -premailer-width: 570px;
-            -premailer-cellpadding: 0;
-            -premailer-cellspacing: 0;
-            text-align: center;
-        }
-
-        .email-footer p {
-            color: #6B6E76;
-        }
-
-        .body-action {
-            width: 100%;
-            margin: 30px auto;
-            padding: 0;
-            -premailer-width: 100%;
-            -premailer-cellpadding: 0;
-            -premailer-cellspacing: 0;
-            text-align: center;
-        }
-
-        .body-sub {
-            margin-top: 25px;
-            padding-top: 25px;
-            border-top: 1px solid #EAEAEC;
-        }
-
-        .content-cell {
-            padding: 35px;
-        }
-
-        /*Media Queries ------------------------------ */
-
-        @media only screen and (max-width: 600px) {
-
-            .email-body_inner,
-            .email-footer {
-                width: 100% !important;
-            }
-        }
-
-        @media (prefers-color-scheme: dark) {
-
-            body,
-            .email-body,
-            .email-body_inner,
-            .email-content,
-            .email-wrapper,
-            .email-masthead,
-            .email-footer {
-                background-color: #333333 !important;
-                color: #FFF !important;
-            }
-
-            p,
-            ul,
-            ol,
-            blockquote,
-            h1,
-            h2,
-            h3,
-            span,
-            .purchase_item {
-                color: #FFF !important;
-            }
-
-            .attributes_content,
-            .discount {
-                background-color: #222 !important;
-            }
-
-            .email-masthead_name {
-                text-shadow: none !important;
-            }
-        }
-
-        :root {
-            color-scheme: light dark;
-            supported-color-schemes: light dark;
-        }
-
     </style>
 </head>
-
 <body>
-    <table class="email-wrapper" width="100%" cellpadding="0" cellspacing="0" role="presentation">
-        <tr>
-            <td align="center">
-                <table class="email-content" width="100%" cellpadding="0" cellspacing="0" role="presentation">
-                    <tr>
-                        <td class="email-masthead">
-                            <a href="https://cdtexpress.com" class="f-fallback email-masthead_name">
-                                <img src="{{ asset('logo.png') }}" alt="logo"
-                                    style="height:100px">
-                            </a>
-                        </td>
-                    </tr>
-                    <!-- Email Body -->
-                    <tr>
-                        <td class="email-body" width="100%" cellpadding="0" cellspacing="0">
-                            <table class="email-body_inner" align="center" width="570" cellpadding="0" cellspacing="0"
-                                role="presentation">
-                                <!-- Body content -->
-                                <tr>
-                                    <td class="content-cell">
-                                        <div class="f-fallback">
-                                       
-                                            <h4>Dear {{ $receiver }},</h4>
-                                            <p>
-                                                We are writing to inform you that we have received a package addressed to you from <strong> {{ $sender }}</strong> .
-                                            </p>
-
-                                            <p>
-                                                <strong>Shipment Details:</strong>
-                                            </p>
-                                            <p>
-                                                <ul>
-                                                    <li>
-                                                        <strong>Tracking Number:</strong> {{$tracking_number}}
-                                                    </li>
-                                                </ul>
-                                                You can track the status of your shipment using the tracking number provided above by visiting our <a href="https://cdtexpress.com/track">tracking page</a>.
-                                                We are processing your shipment and it will be delivered to your address soon.
-                                            </p>
-
-                                            <p>
-                                                If you have any questions or need further assistance, please contact our customer service team at support@cdtexpress.com.
-                                            </p>
-                                        
-                                            <p>
-                                                Thank you for choosing our services. We look forward to delivering your package promptly.
-                                            </p>
-
-                                            <p>
-                                                Best Regards, <br />
-                                                <strong> CD & T Express logistics company!</strong>
-                                                <p>support@cdtexpress.com</p>
-                                                <a  href="https://cdtexpress.com">cdtexpress.com</a>
-                                                <p>371 7th Ave, New York, NY 10001</p>
-                                            </p>
-                                        </div
-                                    </td
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <table class="email-footer" align="center" width="570" cellpadding="0" cellspacing="0"
-                                role="presentation">
-                                <tr>
-                                    <td class="content-cell" align="center">
-                                        <p class="f-fallback sub align-center">&copy; {{ date('Y') }} 
-                                        CD & T Express logistics company. All rights reserved.
-                                        </p>
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
+    <div class="container">
+        <div class="header">
+            <a href="https://cdtexpress.com" class="f-fallback email-masthead_name">
+                <img src="{{ asset('logo.png') }}" alt="logo"style="height:100px">
+            </a>
+        </div>
+        <div class="content">
+            <p>Dear {{$sender}},</p>
+            <p>We are writing to inform you that we have received a package addressed to you from {{$sender}}.</p>
+            <p><strong>Shipment Details:</strong></p>
+            <p><strong>Tracking ID:</strong> {{$tracking_number}}</p>
+            <p>You can check the status of your package using the tracking ID provided above by visiting our <a href="https://cdtexpress.com/track" class="button">Tracking Page</a>.</p>
+            <p>We are processing your shipment and it will be delivered to your address soon.</p>
+            <p>If you have any questions or need further assistance, please contact our customer service team at support@cdtexpress.com.</p>
+            <p>Thank you for choosing our services. We look forward to delivering your package promptly.</p>
+        </div>
+        <div class="footer">
+            <p>Best regards,<br>CD & T Express logistics company!<br>support@cdtexpress.com<br>https://cdtexpress.com<br> 371 7th Ave, New York, NY 10001</p>
+            <p><a href="https://cdtexpress.com/unsubscribe">Unsubscribe</a></p>
+        </div>
+    </div>
 </body>
 </html>
